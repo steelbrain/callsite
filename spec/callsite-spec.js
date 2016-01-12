db1aa50fcf2c7240179b7bfbe8d716711377e02e
steelbrain <aneesiqbalbhatti@gmail.com>
2016-01-13 00:25:39 +0500

    :new: Add another spec for paths without spaces

--- spec/callsite-spec.js
+++ spec/callsite-spec.js
@@ -1,9 +1,19 @@
 describe('sb-callsite', function() {
   const Path = require('path')
 
-  it('works', function() {
+  it('works with spaces', function() {
 
     const fixturePath = Path.join(__dirname, 'fix tures', 'test.js')
+    const stack = require(fixturePath)
+
+    expect(stack[0].file).toBe(fixturePath)
+    expect(stack[0].line).toBe(1)
+
+  })
+
+  it('works without spaces', function() {
+
+    const fixturePath = Path.join(__dirname, 'fixtures', 'test.js')
     const stack = require(fixturePath)
 
     expect(stack[0].file).toBe(fixturePath)
