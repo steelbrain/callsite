9f3cb2182952cc6affe1263a17c063d6e36a924d
Steel Brain <i@steelbrain.me>
2018-12-31 06:46:29 +0500

    :art: Fix ESLint issues

--- spec/callsite-spec.js
+++ spec/callsite-spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require,import/no-dynamic-require */
+
 import test from 'ava'
 import path from 'path'
 
--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 const extractionRegex = /at (\S+) \(([\S ]+):(\d+):(\d+)\)/
 
-export function capture() {
-  const traces = fromStack(new Error().stack)
-  // Remove self from the stack
-  traces.shift()
-  return traces
-}
-
 export function fromStack(stack) {
   const stackChunks = stack.split('\n')
   const traces = []
@@ -17,11 +10,18 @@ export function fromStack(stack) {
       traces.push({
         function: matches[1],
         file: matches[2],
-        line: parseInt(matches[3]) || 1,
-        col: parseInt(matches[4]) || 1,
+        line: parseInt(matches[3], 10) || 1,
+        col: parseInt(matches[4], 10) || 1,
       })
     }
   })
 
+  return traces
+}
+
+export function capture() {
+  const traces = fromStack(new Error().stack)
+  // Remove self from the stack
+  traces.shift()
   return traces
 }
